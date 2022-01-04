6d2c1751ae9fc243f4ca2300aca42d89e2369407
zhangsw <gogozhangsw@gmail.com>
2022-01-04 16:20:43 +0800

    init

---
+++ mall_manager/src/main.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-04 13:00:00
+ * @LastEditTime: 2022-01-04 16:12:36
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /vue2/mall_manager/mall_manager/src/main.js
+ */
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
